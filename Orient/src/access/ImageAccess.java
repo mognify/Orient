9b5242e8d62e47fb2dc0a06b901cdec11e762416
Levi <levimogford@gmail.com>
2019-08-02 12:33:29 -0500

    Update ImageAccess.java

--- Orient/src/access/ImageAccess.java
+++ Orient/src/access/ImageAccess.java
@@ -1,3 +1,5 @@
+package access;
+
 import org.opencv.core.Core;
 import org.opencv.core.Core.MinMaxLocResult;
 import org.opencv.core.Mat;
@@ -5,35 +7,35 @@ import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+
+import orient.Orient;
  
-public class TemplateMatching {
+public class ImageAccess {
  
-    public static void main(String[] args) {
+    public Point screenSearch(String targetName) {
         
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Mat source=null;
         Mat template=null;
-        String filePath="C:\\Users\\mesutpiskin\\Desktop\\Object Detection\\Template Matching\\Sample Image\\";
+        String filePath=Orient.workingDirectory;
         //Load image file
-        source=Imgcodecs.imread(filePath+"kapadokya.jpg");
-        template=Imgcodecs.imread(filePath+"balon.jpg");
+        source=Imgcodecs.imread(filePath+"kapadokya.jpg"); // TODO replace with clipboard screenshot
+        template=Imgcodecs.imread(filePath+targetName+".jpg");
     
         Mat outputImage=new Mat();    
         int machMethod=Imgproc.TM_CCOEFF;
         //Template matching method
         Imgproc.matchTemplate(source, template, outputImage, machMethod);
  
-    
         MinMaxLocResult mmr = Core.minMaxLoc(outputImage);
         Point matchLoc=mmr.maxLoc;
         //Draw rectangle on result image
         Imgproc.rectangle(source, matchLoc, new Point(matchLoc.x + template.cols(),
                 matchLoc.y + template.rows()), new Scalar(255, 255, 255));
  
-        Imgcodecs.imwrite(filePath+"sonuc.jpg", source);
-        System.out.println("Complated.");
+        Imgcodecs.imwrite(filePath+"TemplateMatchingOutput.jpg", source);
+
+	return matchLoc;
     }
  
 }
-
-R
